891612675baf694ed8f65cc626616b8c7b076edf
1hemant <demonvsgod@gmail.com>
2016-09-15 15:21:12 +0530

    02 Save

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 (function () {
+  'use strict';
+   var x = "hello";
   angular.module('myFirstApp', [])
 
-  .controller('MyFirstController', function (){
-
+  .controller('MyFirstController', function ($scope){
+    $scope.name = "Hemant";
+    $scope.sayHello = function () {
+      return "Hello Hemant";
+    };
   });
 })();
